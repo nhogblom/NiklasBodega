76931358b0a19907693bba8a687d714d634f55e9
Niklas Högblom <nhogblom@gmail.com>
2026-05-05 18:48:41 +0200

    Updated UserEntity with variables

--- backend/src/main/java/com/lasias/hostelbookingbackend/models/UserEntity.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/models/UserEntity.java
@@ -1,12 +1,29 @@
 package com.lasias.hostelbookingbackend.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Data
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private List<String> roles;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private String token;
+    //private List<Bookings> listOfBookings;
+
 }
