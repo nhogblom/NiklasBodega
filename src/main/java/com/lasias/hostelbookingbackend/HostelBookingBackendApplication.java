e4ff653d893953e662fc93a47bc6bf588f6da7c1
Niklas Högblom <nhogblom@gmail.com>
2026-05-05 13:09:42 +0200

    Initial commit

---
+++ src/main/java/com/lasias/hostelbookingbackend/HostelBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lasias.hostelbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelBookingBackendApplication.class, args);
+	}
+
+}
