6f166736d63bc3498d77589334ac1ae884aa451f
ivbe0111 <berezkinivan94@gmail.com>
2026-05-05 14:28:20 +0200

    verified that connection to db works. Added H2 database dependency and application.properties for testing

--- backend/src/main/java/com/lasias/hostelbookingbackend/models/UserEntity.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/models/UserEntity.java
@@ -1,4 +1,12 @@
 package com.lasias.hostelbookingbackend.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
