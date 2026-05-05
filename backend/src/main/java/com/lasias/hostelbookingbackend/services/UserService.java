3c1d811a1bea877b6798a0b11ebc0154e0538f6e
Niklas Högblom <nhogblom@gmail.com>
2026-05-05 18:56:34 +0200

    Annotated logging & Service.

--- backend/src/main/java/com/lasias/hostelbookingbackend/services/UserService.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/services/UserService.java
@@ -1,4 +1,9 @@
 package com.lasias.hostelbookingbackend.services;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
 public class UserService {
+
 }
