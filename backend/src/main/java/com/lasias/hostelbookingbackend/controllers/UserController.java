605f6939f5e962920bb2feb06b0623b2c974290c
Niklas Högblom <nhogblom@gmail.com>
2026-05-05 18:56:11 +0200

    Made some initial endpoints for /users ~. No functinality added atm, more work to be done.

--- backend/src/main/java/com/lasias/hostelbookingbackend/controllers/UserController.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/controllers/UserController.java
@@ -1,4 +1,27 @@
 package com.lasias.hostelbookingbackend.controllers;
 
+
+import com.lasias.hostelbookingbackend.dto.AuthResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register() {
+        // do stuff
+        return ResponseEntity.ok().body(new AuthResponse("korv"));
+    }
+
+    @PostMapping("/password")
+    public ResponseEntity<AuthResponse> changePassword(){
+        // do stuff
+        return ResponseEntity.ok().body(new AuthResponse("korv"));
+    }
+
+    
 }
