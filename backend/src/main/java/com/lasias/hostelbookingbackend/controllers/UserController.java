3133c7526d1a2f6f87005187686e9332229faa4e
Niklas Högblom <nhogblom@gmail.com>
2026-05-06 20:50:08 +0200

    Moved AuthResponse to other package.

--- backend/src/main/java/com/lasias/hostelbookingbackend/controllers/UserController.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.lasias.hostelbookingbackend.controllers;
 
 
-import com.lasias.hostelbookingbackend.dto.AuthResponse;
+import com.lasias.hostelbookingbackend.dtos.AuthResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
