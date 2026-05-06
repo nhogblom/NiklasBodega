84a3dc6ccf3360b7bed472176e4c5fd0b535b1a7
Niklas Högblom <nhogblom@gmail.com>
2026-05-06 19:58:40 +0200

    Added register to UserService.

--- backend/src/main/java/com/lasias/hostelbookingbackend/services/UserService.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/services/UserService.java
@@ -1,9 +1,23 @@
 package com.lasias.hostelbookingbackend.services;
+import com.lasias.hostelbookingbackend.models.AppUser;
+import com.lasias.hostelbookingbackend.models.AuthProvider;
+import com.lasias.hostelbookingbackend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
+    public void register(String name, String email, String authProviderId,AuthProvider authProvider){
+        AppUser user = new AppUser();
+        user.setName(name);
+        user.setEmail(email);
+        user.setAuthProvider(authProvider);
+        user.setAuthProviderId(authProviderId);
+        AppUser newlyRegisteredUser = userRepository.save(user);
+    }
 }
