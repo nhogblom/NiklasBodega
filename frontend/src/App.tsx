2d9ec85f76098cb147bff49efcbca6c6f9228566
ivbe0111 <berezkinivan94@gmail.com>
2026-05-06 12:46:40 +0200

    added Forgot password page with dummy code, text forgot password? redirects to /forgotpassword

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,39 @@
-import './App.css'
-import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
-import LoginPage from "./pages/LoginPage.tsx";
-import HomePage from "./pages/HomePage.tsx";
-import RegisterPage from "./pages/RegisterPage.tsx";
-import {useAuth} from "./hooks/useAuth.tsx";
-import MyBookingsPage from "./pages/MyBookingsPage.tsx";
-import NewBookingsPage from "./pages/NewBookingsPage.tsx";
+import './App.css';
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import LoginPage from './pages/LoginPage.tsx';
+import HomePage from './pages/HomePage.tsx';
+import RegisterPage from './pages/RegisterPage.tsx';
+import { useAuth } from './hooks/useAuth.tsx';
+import MyBookingsPage from './pages/MyBookingsPage.tsx';
+import NewBookingsPage from './pages/NewBookingsPage.tsx';
+import ForgotPasswordPage from './pages/ForgotPasswordPage.tsx';
 
 const ProtectedRoute = () => {
-    const { isAuthenticated } = useAuth();
-    return isAuthenticated ? <Outlet /> :  <Navigate to="/login" />;
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
-
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/login" element= {<LoginPage />} />
-              <Route path="/" element={<HomePage/>}/>
-              <Route path="/register" element={<RegisterPage/>}/>
-              <Route element={<ProtectedRoute />}>
-                <Route path="/myBookings" element={<MyBookingsPage/>} />
-                <Route path="/newBooking" element={<NewBookingsPage/>} />
-              </Route>
-          </Routes>
-      </BrowserRouter>
-     )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/myBookings" element={<MyBookingsPage />} />
+          <Route path="/newBooking" element={<NewBookingsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
