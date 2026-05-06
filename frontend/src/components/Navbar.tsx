96a5e7ffc7ad6981a8996b6cee4fd8f23de88011
ivbe0111 <berezkinivan94@gmail.com>
2026-05-06 11:48:27 +0200

    added checks if user is authenticated and buttons based on state to login / homepage

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import {useNavigate} from "react-router-dom";
+import {useAuth} from "../hooks/useAuth.tsx";
 
 
 const Navbar = () => {
 
+    const {isAuthenticated, logout} = useAuth();
     const navigate = useNavigate();
+
 
     return (
     <nav className="flex items-center justify-between px-8 py-6 bg-white border-b border-stone-200 sticky top-0 z-50">
@@ -18,11 +21,22 @@ const Navbar = () => {
             ))}
         </div>
         <div className="flex items-center gap-3">
-            <button
-                onClick={() => navigate('/login')}
-                className="border border-orange-900 text-orange-900 px-6 py-2 rounded-full text-sm font-bold hover:bg-orange-50 transition">
-                LOGIN
-            </button>
+            {isAuthenticated ? (
+                <button
+                    onClick={() => {
+                        logout();
+                        navigate("/");
+                    }}  // what goes here? just logout(), or more?
+                    className="border border-red-700 text-red-700 px-6 py-2 rounded-full text-sm font-bold hover:bg-red-50 transition">
+                    LOGOUT
+                </button>
+            ) : (
+                <button
+                    onClick={() => navigate('/login')}
+                    className="border border-orange-900 text-orange-900 px-6 py-2 rounded-full text-sm font-bold hover:bg-orange-50 transition">
+                    LOGIN
+                </button>
+            )}
             <button className="bg-orange-900 text-white px-6 py-2 rounded-full text-sm font-bold hover:bg-orange-800 transition">
                 BOOK NOW
             </button>
