c698bfc43b885d99c80e79727c2e010dd5dd0259
ivbe0111 <berezkinivan94@gmail.com>
2026-05-06 19:15:10 +0200

    added getAllBookings, createBooking and deleteBooking to BookingApiService

---
+++ frontend/src/api/BookingApiService.ts
@@ -0,0 +1,32 @@
+import axiosInstance from './AxiosConfig.ts';
+import type { Booking } from '../types/Booking.ts';
+
+export const getAllBookings = async () => {
+  try {
+    const response = await axiosInstance.get('/api/bookings/my');
+    return response.data;
+  } catch (error) {
+    console.error('Error getting all user bookings', error);
+    throw error;
+  }
+};
+
+export const createBooking = async (booking: Booking) => {
+  try {
+    const response = await axiosInstance.post('/api/bookings/new', booking);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating new booking', error);
+    throw error;
+  }
+};
+
+export const deleteBooking = async (booking: Booking) => {
+  try {
+    const response = await axiosInstance.delete(`/api/bookings/${booking.id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting booking', error);
+    throw error;
+  }
+};
