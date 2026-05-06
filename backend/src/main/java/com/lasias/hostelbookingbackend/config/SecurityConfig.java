6f317b1876d0222efe0da1d9104b9fbfbb14483e
Niklas Högblom <nhogblom@gmail.com>
2026-05-06 14:42:44 +0200

    SecurityConfig initial Config. Added oAuth2 google & github.

--- backend/src/main/java/com/lasias/hostelbookingbackend/config/SecurityConfig.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.lasias.hostelbookingbackend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +21,14 @@ public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            // todo lägg in rätt saker här för att säkra vårt api
-            return http.build();
+
+            return http
+                    .authorizeHttpRequests(auth -> {
+                        auth.requestMatchers("/api/auth").permitAll();
+                        auth.anyRequest().authenticated();
+                    })
+                    .oauth2Login(withDefaults())
+                    .formLogin(withDefaults())
+                    .build();
         }
 }
