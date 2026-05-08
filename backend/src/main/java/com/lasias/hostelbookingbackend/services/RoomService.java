91a06e867e197e2a7b4b8d97ddaf19d91bd83d27
Viktor Nikander <vikvil91@gmail.com>
2026-05-08 13:22:09 +0200

    add boilerplate methods for RoomService

--- backend/src/main/java/com/lasias/hostelbookingbackend/services/RoomService.java
+++ backend/src/main/java/com/lasias/hostelbookingbackend/services/RoomService.java
@@ -1,7 +1,10 @@
 package com.lasias.hostelbookingbackend.services;
 
+import com.lasias.hostelbookingbackend.models.RoomEntity;
 import com.lasias.hostelbookingbackend.repositories.RoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
@@ -9,5 +12,25 @@ public class RoomService {
 
     public RoomService(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
+    }
+
+    public List<RoomEntity> getRooms() {
+        return null;
+    }
+
+    public List<RoomEntity> addRoom(RoomEntity room) {
+        return null;
+    }
+
+    public RoomEntity getRoomsById(Long id) {
+        return null;
+    }
+
+    public List<RoomEntity> updateRoom(Long id, RoomEntity updatedRoom) {
+        return null;
+    }
+
+    public List<RoomEntity> deleteRoom(Long id) {
+        return null;
     }
 }
