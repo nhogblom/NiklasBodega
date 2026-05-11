0ec98ce4676bf454a233261e8a42895b4c6e1c2c
Niklas Högblom <nhogblom@gmail.com>
2026-05-11 15:28:37 +0200

    Created a test file for AppUserServiceTest.

---
+++ backend/src/test/java/com/lasias/hostelbookingbackend/services/AppUserServiceTest.java
@@ -0,0 +1,53 @@
+package com.lasias.hostelbookingbackend.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void testRegister() {
+    }
+
+    @Test
+    void loginUser() {
+    }
+
+    @Test
+    void validPassword() {
+    }
+
+    @Test
+    void passwordMatchesCriteria() {
+    }
+
+    @Test
+    void hashPassword() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteMe() {
+    }
+
+    @Test
+    void provideUserDetails() {
+    }
+
+    @Test
+    void logout() {
+    }
+}
