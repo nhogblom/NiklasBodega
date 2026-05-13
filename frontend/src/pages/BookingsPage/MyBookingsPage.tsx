0f4cb35b8cab08e56cffc6c55f565bd3d1224ca9
ivbe0111 <berezkinivan94@gmail.com>
2026-05-13 10:36:17 +0200

    broke out components in Booking Page

---
+++ frontend/src/pages/BookingsPage/MyBookingsPage.tsx
@@ -0,0 +1,26 @@
+import { mockBookings } from '../../utils/BookingUtils.ts';
+import BookingPageMainComponent from './components/BookingPageMainComponent.tsx';
+
+const MyBookingsPage = () => {
+  const bookings = mockBookings;
+
+  const handleEdit = (id: number) => {
+    // TODO: navigate to edit booking page
+    console.log('Edit booking', id);
+  };
+
+  const handleCancel = (id: number) => {
+    // TODO: call cancelBooking API then refresh list
+    console.log('Cancel booking', id);
+  };
+
+  return (
+    <BookingPageMainComponent
+      bookings={bookings}
+      handleEdit={handleEdit}
+      handleCancel={handleCancel}
+    />
+  );
+};
+
+export default MyBookingsPage;
